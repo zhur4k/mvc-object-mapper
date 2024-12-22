bb64b3e08cc2b621687e22bbc5c6ff1dbe672e57
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-22 20:40:53 +0300

    feat:create repositories.

--- src/main/java/com/mvc/repository/OrderProductRepository.java
+++ src/main/java/com/mvc/repository/OrderProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
 }
