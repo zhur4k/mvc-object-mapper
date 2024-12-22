9f503fc4f175de7527757dc0a9700cb54b83b978
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-22 19:28:33 +0300

    feat:init project.

---
+++ src/main/java/com/mvc/MvcObjectMapperApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcObjectMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcObjectMapperApplication.class, args);
+    }
+
+}
