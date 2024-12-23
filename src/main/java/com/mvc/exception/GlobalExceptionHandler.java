9aedf4bff2d2c3e86598db8edc1ddb778371ee1c
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-23 07:22:33 +0300

    feat:create NotFoundException and GlobalExceptionHandler.

---
+++ src/main/java/com/mvc/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.mvc.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> globalExceptionHandler(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> globalExceptionHandler(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
